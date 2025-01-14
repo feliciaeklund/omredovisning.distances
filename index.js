5358e3ff8ad499b1f49afeb3291224991f72c112
feliciaeklund <feliciaeklund9@gmail.com>
2025-01-14 09:57:12 +0100

    Funktion för att beräkna avstånd mellan städer
    Tar två städers ID:n och returnerar avståndet mellan dem. Den loopar genom arrayen distances som finns i databasen och letar efter ID:et till den staden som användaren har angivit. Om det blir en match så returneras avståndet och det delas med 10 för att göra om det till mil.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function getDistance(cityId1, cityId2) {
+    for (let space of distances) {
+        if ((space.city1 === cityId1 && space.city2 === cityId2) ||
+            (space.city1 === cityId2 && space.city2 === cityId1)) {
+            return space.distance / 10;
+        }
+    }
+}
 const main = document.querySelector("main");
 const title = document.querySelector("title");
 const cityName = document.querySelector("h2");
